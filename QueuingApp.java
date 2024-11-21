8026d16fe39b2cc08afd8e72618e585e6045b1bc
williamlapena <william.lapena@neu.edu.ph>
2024-11-21 22:46:36 +0800

    QueuingApp.java

--- QueuingApp.java
+++ QueuingApp.java
@@ -1,5 +1,4 @@
 package singletonPattern;
-
 import java.util.Scanner;
 
 public class QueuingApp {
@@ -7,7 +6,6 @@ public class QueuingApp {
         CentralizedQueue queue = CentralizedQueue.getInstance();
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-
         while (!exit) {
             System.out.println("\n--- Pag-Ibig Queue ---\n");
             System.out.println("1. Get Next Queue Number");
@@ -17,9 +15,7 @@ public class QueuingApp {
             System.out.println("5. View Current Queue Number");
             System.out.println("6. Exit");
             System.out.print("Choose an option: ");
-
             int choice = scanner.nextInt();
-
             switch (choice) {
                 case 1:
                     int nextNumber = queue.getNextQueueNumber();
@@ -27,36 +23,29 @@ public class QueuingApp {
                         System.out.println("Next Queue Number: " + nextNumber);
                     }
                     break;
-
                 case 2:
                     System.out.print("Enter Reset Number: ");
                     int resetNumber = scanner.nextInt();
                     queue.resetQueue(resetNumber);
                     break;
-
                 case 3:
                     queue.deactivateQueue();
                     break;
-
                 case 4:
                     queue.reactivateQueue();
                     break;
-
                 case 5:
                     System.out.println("Current Queue Number: " + queue.getCurrentQueueNumber());
                     break;
-
                 case 6:
                     exit = true;
                     System.out.println("Shutting down.");
                     break;
-
                 default:
                     System.out.println("Invalid choice. Please try again.");
                     break;
             }
         }
-
         scanner.close();
     }
 }
